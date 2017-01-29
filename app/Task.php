5276f31b5affc64303da2ad57e8330d767253cd4
wen-chung liao <wcliao1962@gmail.com>
2017-01-29 23:30:52 +0800

    連結task and user model

--- app/Task.php
+++ app/Task.php
@@ -12,4 +12,9 @@ class Task extends Model
      * @var array
      */
     protected $fillable = ['name'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
