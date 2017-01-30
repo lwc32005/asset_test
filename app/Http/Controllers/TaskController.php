192ff98227907f3dbeb25e717b097d919b78ae6b
wen-chung liao <wcliao1962@gmail.com>
2017-01-30 12:50:55 +0800

    認證TaskController，即可認證所有的任務路由

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,7 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 class TaskController extends Controller
 {
-    //
+    /**
+     * 建立一個新的控制器實例。
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
 }
