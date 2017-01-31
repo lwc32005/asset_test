1b7b5ccc6543cf340e8dbefd924f0c23aa706cfc
wen-chung liao <wcliao1962@gmail.com>
2017-01-31 14:40:01 +0800

    連接 Task model與 TaskPolicy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+
+use App\Policies\TaskPolicy;
+use App\Task;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -14,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        Task::class => TaskPolicy::class,
     ];
 
     /**
